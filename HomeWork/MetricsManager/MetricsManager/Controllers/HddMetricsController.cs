8afc60eee159340b448f60b9be34e8b27ef194bd
Valentin Illarionov <i-valentin@mail.ru>
2022-04-19 19:26:11 +0900

    Добавлено логгирование в Менеджер метрик в рамках 3 ДЗ

--- HomeWork/MetricsManager/MetricsManager/Controllers/HddMetricsController.cs
+++ HomeWork/MetricsManager/MetricsManager/Controllers/HddMetricsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,14 +12,24 @@ namespace MetricsManager.Controllers
     [ApiController]
     public class HddMetricsController : ControllerBase
     {
+        private readonly ILogger<HddMetricsController> _logger;
+        public HddMetricsController(ILogger<HddMetricsController> logger)
+        {
+            _logger = logger;
+            _logger.LogDebug(1, "NLog встроен в HddMetricsController");
+        }
+
         [HttpGet("left/agent/{agentId}")]
         public IActionResult GetFreeHDDSpaceFromAgent([FromRoute] int agentId)
         {
+            _logger.LogInformation($"Список свободного места от агента {agentId} передан");
             return Ok();
         }
+
         [HttpGet("left/cluster")]
         public IActionResult GetFreeHDDSpaceFromAllCluster()
         {
+            _logger.LogInformation($"Список свободного места от кластера передан");
             return Ok();
         }
     }
